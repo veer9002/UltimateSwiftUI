7a039217584efa998f1fc7abc473a318db871dbf
Veer9002 <mann_9898771157@yahoo.com>
2023-12-06 23:59:05 +0530

    complete Calculator functionality

--- UltimateSwiftUI/CalculatorView.swift
+++ UltimateSwiftUI/CalculatorView.swift
@@ -9,6 +9,24 @@ import SwiftUI
 
 struct CalculatorView: View {
     @State var textResult: String = "0"
+    @State var firstNumber: Double = 0 {
+        willSet {
+            textResult = "\(newValue)"
+        }
+    }
+    @State var secondNumber: Double = 0 {
+        willSet {
+            textResult = "\(newValue)"
+        }
+    }
+    @State var operation: String = "AC" {
+        willSet {
+            performOperationWillSet(newValue: newValue, oldValue: self.operation)
+        }
+        didSet {
+            performOperationDidSet(operation: oldValue)
+        }
+    }
     
     var body: some View {
         VStack(spacing: 20) {
@@ -21,106 +39,240 @@ struct CalculatorView: View {
                 )
             HStack(spacing: 20) {
                 Button {
-                    textResult = "0"
+                    operation = "AC"
                 } label: {
                     CalculatorButtonLayout(text: "AC", textColor: .white, backgroundColor: .gray)
                 }
                 Button {
-                    textResult = "0"
+                    operation = "+/-"
                 } label: {
                     CalculatorButtonLayout(text: "+/-", textColor: .white, backgroundColor: .gray)
                 }
                 Button {
-                    textResult = "0"
+                    operation = "%"
                 } label: {
                     CalculatorButtonLayout(text: "%", textColor: .white, backgroundColor: .gray)
                 }
                 Button {
-//                    textResult = "0"
+                    operation = "+"
                 } label: {
-                    CalculatorButtonLayout(text: "/", textColor: .white, backgroundColor: .orange)
-                }
-            }
-            HStack(spacing: 20) {
-                Button {
-                    textResult = "7"
-                } label: {
-                    CalculatorButtonLayout(text: "7", textColor: .white, backgroundColor: Color(.darkGray))
-                }
-                Button {
-                    textResult = "8"
-                } label: {
-                    CalculatorButtonLayout(text: "8", textColor: .white, backgroundColor: Color(.darkGray))
-                }
-                Button {
-                    textResult = "9"
-                } label: {
-                    CalculatorButtonLayout(text: "9", textColor: .white, backgroundColor: Color(.darkGray))
-                }
-                Button {
-                    textResult = "0"
-                } label: {
-                    CalculatorButtonLayout(text: "x", textColor: .white, backgroundColor: .orange)
+                    CalculatorButtonLayout(text: "÷", textColor: .white, backgroundColor: .orange)
                 }
             }
             HStack(spacing: 20) {
                 
                 Button {
-                    textResult = "4"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 7
+                        } else {
+                            firstNumber = firstNumber * 10 + 7
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 7
+                        } else {
+                            secondNumber = secondNumber * 10 + 7
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "4", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "7", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "5"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 8
+                        } else {
+                            firstNumber = firstNumber * 10 + 8
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 8
+                        } else {
+                            secondNumber = secondNumber * 10 + 8
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "5", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "8", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "6"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 9
+                        } else {
+                            firstNumber = firstNumber * 10 + 9
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 9
+                        } else {
+                            secondNumber = secondNumber * 10 + 9
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "6", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "9", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "-"
+                    operation = "x"
+                } label: {
+                    CalculatorButtonLayout(text: "×", textColor: .white, backgroundColor: .orange)
+                }
+            }
+            
+            HStack(spacing: 20) {
+                Button {
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 4
+                        } else {
+                            firstNumber = firstNumber * 10 + 4
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 4
+                        } else {
+                            secondNumber = secondNumber * 10 + 4
+                        }
+                    }
+                } label: {
+                    CalculatorButtonLayout(text: "4", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
+                }
+
+                Button {
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 5
+                        } else {
+                            firstNumber = firstNumber * 10 + 5
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 5
+                        } else {
+                            secondNumber = secondNumber * 10 + 5
+                        }
+                    }
+                } label: {
+                    CalculatorButtonLayout(text: "5", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
+                }
+
+                Button {
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 6
+                        } else {
+                            firstNumber = firstNumber * 10 + 6
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 6
+                        } else {
+                            secondNumber = secondNumber * 10 + 6
+                        }
+                    }
+                } label: {
+                    CalculatorButtonLayout(text: "6", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
+                }
+
+                Button {
+                    operation = "-"
                 } label: {
                     CalculatorButtonLayout(text: "-", textColor: .white, backgroundColor: .orange)
                 }
             }
+            
             HStack(spacing: 20) {
                 Button {
-                    textResult = "1"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 1
+                        } else {
+                            firstNumber = firstNumber * 10 + 1
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 1
+                        } else {
+                            secondNumber = secondNumber * 10 + 1
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "1", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "1", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "2"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 2
+                        } else {
+                            firstNumber = firstNumber * 10 + 2
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 2
+                        } else {
+                            secondNumber = secondNumber * 10 + 2
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "2", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "2", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "3"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 3
+                        } else {
+                            firstNumber = firstNumber * 10 + 3
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 3
+                        } else {
+                            secondNumber = secondNumber * 10 + 3
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "3", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: "3", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "0"
+                    operation = "+"
                 } label: {
                     CalculatorButtonLayout(text: "+", textColor: .white, backgroundColor: .orange)
                 }
             }
+            
             HStack(spacing: 20) {
                 Button {
-                    textResult = "0"
+                    if operation == "AC" {
+                        if firstNumber == 0 {
+                            firstNumber = 0
+                        } else {
+                            firstNumber = firstNumber * 10 + 0
+                        }
+                    } else {
+                        if secondNumber == 0 {
+                            secondNumber = 1
+                        } else {
+                            secondNumber = secondNumber * 10
+                        }
+                    }
                 } label: {
-                    CalculatorButtonLayout(text: "0", textColor: .white, backgroundColor: Color(.darkGray), width: 180)
+                    CalculatorButtonLayout(text: "0", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75), width: 180)
                 }
+
                 Button {
-                    textResult = ""
+                    
                 } label: {
-                    CalculatorButtonLayout(text: ",", textColor: .white, backgroundColor: Color(.darkGray))
+                    CalculatorButtonLayout(text: ",", textColor: .white, backgroundColor: Color(uiColor: .darkGray).opacity(0.75))
                 }
+
                 Button {
-                    textResult = "="
+                    operation = "="
                 } label: {
                     CalculatorButtonLayout(text: "=", textColor: .white, backgroundColor: .orange)
                 }
@@ -128,6 +280,44 @@ struct CalculatorView: View {
         }
         .preferredColorScheme(.dark)
     }
+    
+    func performOperationWillSet() {
+        
+    }
+    
+    func performOperationDidSet(operation: String) {
+        if operation == "+" {
+            let result = firstNumber + secondNumber
+            secondNumber = 0
+            firstNumber = result
+        } else if operation == "-" {
+            let result = firstNumber - secondNumber
+            secondNumber = 0
+            firstNumber = result
+        } else if operation == "x" {
+            let result = firstNumber * secondNumber
+            secondNumber = 0
+            firstNumber = result
+        } else if operation == "÷" {
+            let result = firstNumber / secondNumber
+            secondNumber = 0
+            firstNumber = result
+        }
+    }
+    
+    func performOperationWillSet(newValue: String, oldValue: String) {
+        if newValue == "+/-" {
+            performOperationDidSet(operation: oldValue)
+            firstNumber = -firstNumber
+        } else if newValue == "%" {
+            performOperationDidSet(operation: oldValue)
+            firstNumber = firstNumber / 100
+        } else if newValue == "AC" {
+            firstNumber = 0
+            secondNumber = 0
+        }
+    }
+    
 }
 
 struct CalculatorButtonLayout: View {
